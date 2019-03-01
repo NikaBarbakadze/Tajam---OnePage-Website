d3bb6acbf3b86208c918112fb7906af371ff8ee5
NikaBarbakadze <45419134+NikaBarbakadze@users.noreply.github.com>
2019-03-01 14:37:16 -0800

    Full version with prefixer

---
+++ scripts/js/main.js
@@ -0,0 +1,12 @@
+// For Menu Button
+    function myFunction() {
+      var element = document.getElementById("hamburger_menu");
+      element.classList.toggle("active");
+      var element = document.getElementById("burger-list");
+      element.classList.toggle("active"); 
+      var element = document.getElementById("container");
+      element.classList.toggle("change");}
+
+
+
+
